776f6847f431dc8cc23ce9804e205706656f81e5
pfilmintz <pmintah7@gmail.com>
2022-08-03 13:12:29 +0000

    Added Transfers Tab

---
+++ expressPay Clone/Transfers/TransferItemCell.swift
@@ -0,0 +1,76 @@
+//
+//  TransferItemCell.swift
+//  expressPay Clone
+//
+//  Created by mac on 03/08/2022.
+//
+
+import UIKit
+
+class TransferItemCell: UICollectionViewCell {
+    
+    static let identifier = "TransferItemCell"
+    
+    var imgView: UIImageView = {
+        let image = UIImageView()
+        
+        
+        image.clipsToBounds = true
+        
+        return image
+    }()
+    
+    var nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Avenir", size: 16)
+        label.textColor = .black
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        
+        return label
+    }()
+    
+    required init?(coder: NSCoder) {
+        
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+   
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        
+        self.clipsToBounds = false
+        
+        contentView.addSubview(imgView)
+  
+        contentView.addSubview(nameLabel)
+        
+
+        
+        
+        setupConstrains()
+        
+    }
+    
+    func setupConstrains(){
+        
+        
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        imgView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            
+            imgView.centerXAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.centerXAnchor),
+            imgView.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 8),
+            imgView.heightAnchor.constraint(equalToConstant: 50),
+            imgView.widthAnchor.constraint(equalToConstant: 50),
+            
+            nameLabel.leftAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leftAnchor, constant: 8),
+            nameLabel.rightAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.rightAnchor, constant: -8),
+            nameLabel.topAnchor.constraint(equalTo: imgView.bottomAnchor, constant: 8)
+            
+            
+            ])
+    }
+}
--- expressPay Clone/Transfers/TransfersVC.swift
+++ expressPay Clone/Transfers/TransfersVC.swift
@@ -7,12 +7,129 @@
 
 import UIKit
 
-class TransfersVC: UIViewController {
+class TransfersVC: UIViewController , UICollectionViewDelegate,UICollectionViewDataSource {
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TransferItemCell.identifier , for: indexPath) as! TransferItemCell
+        
+        
+        
+        let image = items[indexPath.row].image
+        let name = items[indexPath.row].title
+        
+        cell.nameLabel.text = name
+        
+        cell.imgView.image = image
+        
+       
+        
+        return cell
+    }
+    
+    
+    
+    let layout = UICollectionViewFlowLayout()
+    
+    
+    
+    private var collectionview: UICollectionView?
+   
+    struct ItemData {
+        let title: String
+        let image: UIImage
+    }
+    
+    var items = [ItemData]()
+    
+    var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Where would you like to send the funds to?"
+        label.font = UIFont(name: "Avenir", size: 22)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        label.textColor = #colorLiteral(red: 0, green: 0.6226729155, blue: 1, alpha: 1)
+        
+        return label
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        view.addSubview(titleLabel)
+        
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8).isActive = true
+        
+        titleLabel.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -16).isActive = true
+        titleLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 16).isActive = true
 
-        // Do any additional setup after loading the view.
+        items.append(ItemData(title: "MTN Mobile Money", image:  #imageLiteral(resourceName: "groceries")))
+        items.append(ItemData(title: "Bank Direct", image:  #imageLiteral(resourceName: "groceries")))
+        items.append(ItemData(title: "AirtelTigo Money", image:  #imageLiteral(resourceName: "groceries")))
+        items.append(ItemData(title: "Vodafone Cash", image:  #imageLiteral(resourceName: "groceries")))
+        items.append(ItemData(title: "Visa Card", image:  #imageLiteral(resourceName: "groceries")))
+      
+        items.append(ItemData(title: "expressCard", image:  #imageLiteral(resourceName: "groceries")))
+        items.append(ItemData(title: "myQR", image:  #imageLiteral(resourceName: "groceries")))
+        
+       // view.backgroundColor = .systemBackground
+        
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 20
+        layout.minimumInteritemSpacing = 8
+        
+        let width = (view.frame.size.width/3) - 16
+        let height = width
+        
+        layout.itemSize = CGSize(width: width,
+                                 height: height)
+        collectionview = UICollectionView(frame: .zero, collectionViewLayout: layout)
+      
+        
+      
+      
+        
+        guard let collectionview = collectionview else {
+            return
+        }
+
+        collectionview.register(TransferItemCell.self, forCellWithReuseIdentifier: TransferItemCell.identifier)
+     
+        title = "collectionView"
+        
+        collectionview.delegate = self
+        collectionview.dataSource = self
+        
+     
+        
+        view.addSubview(collectionview)
+        
+        collectionview.backgroundColor = .white
+         
+        collectionview.translatesAutoresizingMaskIntoConstraints = false
+         
+         
+        collectionview.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16).isActive = true
+        collectionview.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        collectionview.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -16).isActive = true
+        collectionview.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 16).isActive = true
+        
+        collectionview.reloadData()
     }
     
 
